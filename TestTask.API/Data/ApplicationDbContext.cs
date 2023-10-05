8fb83e39911d14e12e9c506f377d10cbfe74a993
andrw04 <sivyandrey2021@gmail.com>
2023-10-05 17:04:58 +0300

    Add project files.

---
+++ TestTask.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Domain.Entities;
+
+namespace TestTask.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set;  }
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+    }
+}
---
+++ TestTask.API/Data/DbInitialize.cs
@@ -0,0 +1,44 @@
+﻿using TestTask.Domain.Entities;
+
+namespace TestTask.API.Data
+{
+    public class DbInitialize
+    {
+        public static async Task SeedData(WebApplication app)
+        {
+            await Task.Run(() =>
+            {
+                using var scope = app.Services.CreateScope();
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                if (!context.Roles.Any())
+                {
+                    context.Roles.AddRange(new List<Role>
+                    {
+                        new Role { Name = "User" },
+                        new Role { Name = "Admin" },
+                        new Role { Name = "Support" },
+                        new Role { Name = "SuperAdmin" },
+                    });
+
+                    context.SaveChanges();
+                }
+
+                if (!context.Users.Any())
+                {
+                    context.Users.AddRange(new List<User>
+                    {
+                        new User { Name = "User1", Age = 31, Email = "user1@gmail.com" },
+                        new User { Name = "User2", Age = 39, Email = "user2@gmail.com" },
+                        new User { Name = "User3", Age = 18, Email = "user3@gmail.com" },
+                    });
+
+                    context.SaveChanges();
+                }
+            });
+        }
+    }
+}
